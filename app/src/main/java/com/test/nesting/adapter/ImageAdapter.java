66c816bcfd5b4bd2ee4d3dbb0710b03d29dcbc59
zhenxing <zhenxing@qh-speed.com>
2017-03-20 18:14:38 +0800

    aaa

--- app/src/main/java/com/test/nesting/adapter/ImageAdapter.java
+++ app/src/main/java/com/test/nesting/adapter/ImageAdapter.java
@@ -8,7 +8,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
-
 import com.bumptech.glide.Glide;
 import com.test.view.BaseAdapter;
 import com.test.zhyrecycleview.R;
